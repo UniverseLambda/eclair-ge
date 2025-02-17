463c1985018a304b8789d3feff2b5cab5355ee3d
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2025-02-17 16:41:26 +0100

    add some function keywords

--- ege-transpiler/src/lexer/mod.rs
+++ ege-transpiler/src/lexer/mod.rs
@@ -13,12 +13,13 @@ const KEYWORDS: [&str; 36] = [
     "Type", "Field", "Function", "Return", "And", "Or", "Not", // Syntaxical keywords
 ];
 
-const FUNCTION_KEYWORDS: [&str; 11] = [
+const FUNCTION_KEYWORDS: [&str; 14] = [
     "Print", "AppTitle", "Graphics", "Cls", "Text",
-    "PlaySound",
+    "DrawImage", "MaskImage",
+    "PlaySound", "PlaySound_Strict",
     "FreeEntity", "PositionEntity", "ResetEntity", "ShowEntity",
     "AASetFont",
-    ];
+];
 
 const OPERATORS: [&str; 16] = [
     "(", ")", "=", "<>", "<", ">", "<=", ">=", "+", "-", "*", "/", "^", ",", ".", ":",
