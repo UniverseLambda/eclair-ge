ac3a85793c82c5cd04fd664bd4af728186b02fc0
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2025-02-17 23:42:41 +0100

    Move test files to a directory, fix include to be relative to the current file

--- ege-transpiler/src/parser/ast_type/include.rs
+++ ege-transpiler/src/parser/ast_type/include.rs
@@ -1,26 +1,41 @@
+use std::path::Path;
+
 use serde::Serialize;
 
-use crate::{lexer::{TokenTypeId, Tokenizer}, parser::{ext::TokenExt, Parser}};
+use crate::{
+    lexer::{TokenTypeId, Tokenizer},
+    parser::{ext::TokenExt, Parser},
+};
 
 use super::{Parsable, Program};
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Include {
-	pub program: Program
+    pub program: Program,
 }
 
 impl Parsable for Include {
-	fn parse(parser: &mut Parser<impl std::io::Read>) -> anyhow::Result<Self> {
-		parser.required_token().expect_token(TokenTypeId::Keyword, "Include")?;
+    fn parse(parser: &mut Parser<impl std::io::Read>) -> anyhow::Result<Self> {
+        let include_token = parser
+            .required_token()
+            .expect_token(TokenTypeId::Keyword, "Include")?;
 
-		let path = parser.required_next_token().expect_type(TokenTypeId::StringLiteral)?.content;
-		parser.consume_token();
+        let path = parser
+            .required_next_token()
+            .expect_type(TokenTypeId::StringLiteral)?
+            .content;
+        parser.consume_token();
 
-		let file = std::fs::File::open(&path)?;
+        let real_file_path = Path::new(&include_token.source_path)
+            .parent()
+            .unwrap()
+            .join(path);
 
-		let lexer = Tokenizer::new(path, file);
-		let program = Parser::new(lexer).parse_program()?;
+        let file = std::fs::File::open(&real_file_path)?;
 
-		Ok(Self { program })
-	}
+        let lexer = Tokenizer::new(real_file_path.to_string_lossy().into_owned(), file);
+        let program = Parser::new(lexer).parse_program()?;
+
+        Ok(Self { program })
+    }
 }
